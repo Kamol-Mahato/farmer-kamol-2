ac6e19733219fa14988068f34f0fd673a762e7cb
Kamol Mahato <99003107+Kamol-Mahato@users.noreply.github.com>
2026-08-16 07:43:48 +0000

    Image OPT

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,7 @@ const nextConfig = {
   allowedDevOrigins: ['3000-firebase-farmer-kamolgit-1781445602919.cluster-edb2jv34dnhjisxuq5m7l37ccy.cloudworkstations.dev'],
   poweredByHeader: false,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
